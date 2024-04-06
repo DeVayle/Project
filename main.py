18523f8ccfd9f08af8c2fbab926e440d29c9b3db
DeVayle <dmitrydolgikh.01@gmail.com>
2024-04-06 22:30:29 +0300

    Update

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ import winsound
 
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
 platforms = []
+exits = []
+pressed_keys = set()
+
+def key_pressed(event):
+    pressed_keys.add(event.keysym)
+
+def key_released(event):
+    pressed_keys.remove(event.keysym)
 
 
 def clear_window():
@@ -58,7 +66,7 @@ def restore_menu():
 
 
 def main_menu():
-    lbl_gamename = tk.Label(window, text="SkyBound", font=btn_font)
+    lbl_gamename = tk.Label(window, text="AeroCube", font=btn_font)
     lbl_gamename.place(anchor="center", relx=.2, rely=.2, relheight=.1, relwidth=.2)
 
     btn_play = tk.Button(window, text="Играть", command=level_1, font=btn_font)
@@ -71,15 +79,13 @@ def main_menu():
     btn_exit.place(anchor="center", relx=.15, rely=.72, relheight=.1, relwidth=.2)
 
 
-def move_left(event):
+def move():
     x1, y1, x2, y2 = character_canvas.coords(character)
-    if x1 > 0 and check_collision(x1, y1, x2, y2) != 'left_or_right':  # Проверка, находится ли персонаж у левой границы
-        character_canvas.move(character, -10, 0)
-
-def move_right(event):
-    x1, y1, x2, y2 = character_canvas.coords(character)
-    if x2 < window.winfo_width() and check_collision(x1, y1, x2, y2) != 'left_or_right':  # Проверка, находится ли персонаж у правой границы
-        character_canvas.move(character, 10, 0)
+    if (x1 > 0 and check_collision_platforms(x1, y1, x2, y2)) or (x2 < window.winfo_width() and check_collision_platforms(x1, y1, x2, y2)) != 'left_or_right':
+        vel = -10 * ("Left" in pressed_keys) + 10 * ("Right" in pressed_keys)
+        character_canvas.move(character, vel, 0)
+        check_collision_exits()
+        window.after(25, move)  # Повторить перемещение через 50 мс
 
 # Инициализация возможных описаний персонажа "В воздухе" и "В прыжке"
 in_air = False
@@ -98,7 +104,7 @@ def jump_up(initial_y):
     x1, y1, x2, y2 = character_canvas.coords(character)
     if y1 > initial_y - 300:  # Проверка, достиг ли персонаж во время прыжка своей максимальной высоты прыжка
         character_canvas.move(character, 0, -15)
-        window.after(50, lambda: jump_up(initial_y))  # Вызов функции еще раз после задержки
+        window.after(15, lambda: jump_up(initial_y))  # Вызов функции еще раз после задержки
     else:
         jumping = False
 
@@ -109,7 +115,7 @@ def apply_gravity():
     x1, y1, x2, y2 = character_canvas.coords(character)
 
     # Проверка, находится ли персонаж на земле или на платформе
-    if y2 < window.winfo_height() and check_collision(x1, y1, x2, y2) != 'top_or_bottom':
+    if y2 < window.winfo_height() and check_collision_platforms(x1, y1, x2, y2) != 'top_or_bottom':
         # Если персонаж находится в воздухе, начать опускать его вниз (эффект гравитации)
         character_canvas.move(character, 0, 15)
         in_air = True
@@ -117,10 +123,10 @@ def apply_gravity():
         in_air = False
         jumping = False
     # Вызов функции еще раз после задержки
-    window.after(100, apply_gravity)
+    window.after(30, apply_gravity)
 
 
-def check_collision(x1, y1, x2, y2):
+def check_collision_platforms(x1, y1, x2, y2):
     for platform in platforms:
         px1, py1, px2, py2 = character_canvas.coords(platform)
         if (px1 < x1 < px2 or px1 < x2 < px2) and (py1 - 10 < y2 < py1 + 10 or py1 - 10 < y1 < py1 + 10):
@@ -129,17 +135,31 @@ def check_collision(x1, y1, x2, y2):
             return 'left_or_right'
     return None
 
+def check_collision_exits():
+    x1, y1, x2, y2 = character_canvas.coords(character)
+    for exit in exits:
+        ex1, ey1, ex2, ey2 = character_canvas.coords(exit)
+        if (ex1 < x1 < ex2 or ex1 < x2 < ex2) and (ey1 - 10 < y2 < ey1 + 10 or ey1 - 10 < y1 < ey1 + 10):
+            level_2()
+
 
 def create_character():
     global character_canvas, character
     character_canvas = tk.Canvas(window, bg="DarkGray", width=1920, height=1080)
     character_canvas.place(anchor="center", relx=.5, rely=.5)
+
+    btn_pause = tk.Button(window, text="II", command=restore_menu, font=btn_font)
+    btn_pause.place(anchor="center", relx=.026, rely=.046, relwidth=.03125, relheight=.05)
+
     character = character_canvas.create_rectangle(800, 1020, 860, 1080, fill="blue")
 
     # Назначение клавиш для функций передвижения
-    window.bind("<Left>", move_left)
-    window.bind("<Right>", move_right)
+    move()
     window.bind("<Up>", move_up)
+
+    window.bind_all('<KeyRelease>', key_released)
+    window.bind_all('<KeyPress>', key_pressed)
+
     apply_gravity()
 
 
@@ -149,16 +169,24 @@ def create_platform(x, y, width, height):
     platforms.append(platform)
 
 
+def create_exit(x, y, width, height):
+    exit = character_canvas.create_rectangle(x, y, x + width, y + height, fill="red")
+    exits.append(exit)
+
+
 def level_1():
     clear_window()
     create_character()
 
-    btn_pause = tk.Button(window, text="II", command=restore_menu, font=btn_font)
-    btn_pause.place(anchor="center", relx=.026, rely=.046, relwidth=.03125, relheight=.05)
-
     create_platform(100, 900, 200, 20)
     create_platform(500, 650, 100, 20)
     create_platform(1400, 700, 150, 20)
+    create_exit(1800, 1020, 100, 100)
+
+
+def level_2():
+    clear_window()
+    create_character()
 
 
 
